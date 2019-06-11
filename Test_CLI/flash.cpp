ca6f0229eddfdeec62416cfc417313dc6513de45
andreadbyfive <andreadbyfive@gmail.com>
2019-06-11 14:03:30 +0900

    Flash add

---
+++ Test_CLI/flash.cpp
@@ -0,0 +1,263 @@
+#include "stdafx.h"
+#include <windows.h>
+#include "CLI.h"
+#include "flash.h"
+#include <time.h>
+
+#define FLASH_TIME_OUT_SECOND 3
+#define SFLS_REG_BASE 0x40500000
+#define REG_SIZE 4
+
+#define BUS_REQ 1
+#define USR_BUS_REQ 3
+UINT32 sfls_vait_for_reg_request(int fd)
+{
+	UINT32 read = 0;
+	UINT32 err = 0;
+	clock_t start;
+	start = clock();
+	do{
+		Sleep(100);
+		getDataRSP_(fd, SFLS_REG_BASE+(8<<3), REG_SIZE, &read);
+		
+		if (((clock() - start) / CLOCKS_PER_SEC) > FLASH_TIME_OUT_SECOND) return -1;
+	} while (read & (1 << BUS_REQ));
+
+	return 0;
+}
+
+#define ID_VAL 0
+UINT32 sfls_vait_for_reg_idval(int fd)
+{
+	UINT32 read = 0;
+	UINT32 err = 0;
+	clock_t start;
+	start = clock();
+	do{
+		Sleep(100);
+		getDataRSP_(fd, SFLS_REG_BASE + (0 << 3), REG_SIZE, &read);
+
+		if (((clock() - start) / CLOCKS_PER_SEC) > FLASH_TIME_OUT_SECOND) return -1;
+	} while (!(read & (1 << ID_VAL)));
+
+	return 0;
+}
+
+void sfls_init(int fd)
+{
+	sfls_vait_for_reg_idval(fd);
+
+	_SFLS_1 sf1;
+	getDataRSP_(fd, SFLS_REG_BASE + (1 << 3), REG_SIZE, &sf1);
+	printf("get sf1 %x \n", sf1);
+	sf1.BUS_CMD_RD = 0xEB;
+	sf1.BUS_CMD_WR = 0x02;
+	sf1.BUS_CMD_WREN = 0x06;
+	sf1.BUS_CMD_RDREG = 0x05;
+	printf("set sf1 %x \n", sf1);
+	setDataRSP_(fd, SFLS_REG_BASE + (1 << 3), REG_SIZE, &sf1);
+
+	_SFLS_2 sf2;
+	getDataRSP_(fd, SFLS_REG_BASE + (2 << 3), REG_SIZE, &sf2);
+	printf("get sf2 %x \n", sf2);
+	sf2.BUS_RD_CMD_MODE = 2;
+	sf2.BUS_RD_ADR_MODE = 2;
+	sf2.BUS_RD_DAT_MODE = 2;
+	sf2.BUS_WR_CMD_MODE = 2;
+	sf2.BUS_WR_ADR_MODE = 2;
+	sf2.BUS_WR_DAT_MODE = 2;
+	sf2.BUS_WREN_CMD_MODE = 2;
+	sf2.BUS_RDREG_CMD_MODE = 2;
+	sf2.BUS_RDREG_DAT_MODE = 2;
+	sf2.BUS_GAP_EN = 1;
+	sf2.BUS_GAP = 1; // Flash 마다 다름 Winbond 1  Eon 5 
+	printf("set sf2 %x \n", sf2);
+	setDataRSP_(fd, SFLS_REG_BASE + (2 << 3), REG_SIZE, &sf2);
+
+	_SFLS_4 sf4;
+	getDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+	printf("get sf4 %x \n", sf4);
+	sf4.USR_CMD = 0x38;
+	printf("set sf4 %x \n", sf4);
+	setDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+
+	_SFLS_8 sf8;
+	getDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+	printf("get sf8 %x \n", sf8);
+	sf8.IO_CLKDIV = 1;
+	sf8.IO_RDLTC = 2;
+	sf8.USRBUS_REQ = 1;
+	printf("set sf8 %x \n", sf8);
+	setDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+
+	sfls_vait_for_reg_request(fd);
+
+}
+
+void sfls_write_enable(int fd)
+{
+	_SFLS_4 sf4;
+	getDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+	printf("get sf4 %x \n", sf4);
+	sf4.USR_CMD_MODE = 2;
+	sf4.USR_ADR_EN = 0;
+	sf4.USR_GAP_EN = 0;
+	sf4.USR_RD_EN = 0;
+	sf4.USR_WR_EN = 0;
+	sf4.USR_GAP = 0;
+	sf4.USR_LEN = 0;
+	sf4.USR_ADR_EXT = 0;
+	sf4.USR_BUSY_EN = 0;
+	sf4.USR_RDLTC = 2;
+	sf4.USR_CMD = 0x06;
+	printf("set sf4 %x \n", sf4);
+	setDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+
+	_SFLS_8 sf8;
+	getDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+	printf("get sf8 %x \n", sf8);
+	sf8.USR_REQ = 1;
+	printf("set sf8 %x \n", sf8);
+	setDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+
+	sfls_vait_for_reg_request(fd);
+}
+
+void sfls_sect_erase(int fd, UINT Adr)
+{
+	sfls_write_enable(fd);
+
+	_SFLS_4 sf4;
+	getDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+	printf("get sf4 %x \n", sf4);
+	sf4.USR_CMD_MODE = 2;
+	sf4.USR_ADR_MODE = 2;
+	sf4.USR_ADR_EN = 1;
+	sf4.USR_GAP_EN = 0;
+	sf4.USR_RD_EN = 0;
+	sf4.USR_WR_EN = 0;
+	sf4.USR_GAP = 0;
+	sf4.USR_LEN = 0;
+	sf4.USR_ADR_EXT = 0;
+	sf4.USR_BUSY_EN = 1;
+	sf4.USR_RDLTC = 2;
+	sf4.USR_CMD = 0x20;
+	printf("set sf4 %x \n", sf4);
+	setDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+
+	_SFLS_3 sf3;
+	getDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+	printf("get sf3 %x \n", sf3);
+	sf3.USR_RDREG_CMD_MODE = 2;
+	sf3.USR_RDREG_DAT_MODE = 2;
+	printf("set sf3 %x \n", sf3);
+	setDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+
+	_SFLS_5 sf5;
+	getDataRSP_(fd, SFLS_REG_BASE + (5 << 3), REG_SIZE, &sf5);
+	printf("get sf5 %x \n", sf5);
+	sf5.USR_ADR = Adr;
+	printf("set sf5 %x \n", sf5);
+	setDataRSP_(fd, SFLS_REG_BASE + (5 << 3), REG_SIZE, &sf5);
+
+	_SFLS_8 sf8;
+	getDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+	printf("get sf8 %x \n", sf8);
+	sf8.USR_REQ = 1;
+	printf("set sf8 %x \n", sf8);
+	setDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+
+	sfls_vait_for_reg_request(fd);
+
+}
+
+void sfls_block_erase(int fd, UINT Adr)
+{
+	sfls_write_enable(fd);
+
+	_SFLS_4 sf4;
+	getDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+	printf("get sf4 %x \n", sf4);
+	sf4.USR_CMD_MODE = 2;
+	sf4.USR_ADR_MODE = 2;
+	sf4.USR_ADR_EN = 1;
+	sf4.USR_GAP_EN = 0;
+	sf4.USR_RD_EN = 0;
+	sf4.USR_WR_EN = 0;
+	sf4.USR_GAP = 0;
+	sf4.USR_LEN = 0;
+	sf4.USR_ADR_EXT = 0;
+	sf4.USR_RDLTC = 2;
+	sf4.USR_BUSY_EN = 1;
+	sf4.USR_CMD = 0xd8;
+	printf("set sf4 %x \n", sf4);
+	setDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+
+	_SFLS_3 sf3;
+	getDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+	printf("get sf3 %x \n", sf3);
+	sf3.USR_RDREG_CMD_MODE = 2;
+	sf3.USR_RDREG_DAT_MODE = 2;
+	printf("set sf3 %x \n", sf3);
+	setDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+
+	_SFLS_5 sf5;
+	getDataRSP_(fd, SFLS_REG_BASE + (5 << 3), REG_SIZE, &sf5);
+	printf("get sf5 %x \n", sf5);
+	sf5.USR_ADR = Adr;
+	printf("set sf5 %x \n", sf5);
+	setDataRSP_(fd, SFLS_REG_BASE + (5 << 3), REG_SIZE, &sf5);
+
+	_SFLS_8 sf8;
+	getDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+	printf("get sf8 %x \n", sf8);
+	sf8.USR_REQ = 1;
+	printf("set sf8 %x \n", sf8);
+	setDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+
+	sfls_vait_for_reg_request(fd);
+
+}
+
+void sfls_chip_erase(int fd)
+{
+	sfls_write_enable(fd);
+
+	_SFLS_4 sf4;
+	getDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+	printf("get sf4 %x \n", sf4);
+	sf4.USR_CMD_MODE = 2;
+
+	sf4.USR_ADR_EN = 0;
+	sf4.USR_GAP_EN = 0;
+	sf4.USR_RD_EN = 0;
+	sf4.USR_WR_EN = 0;
+	sf4.USR_GAP = 0;
+	sf4.USR_LEN = 0;
+	sf4.USR_ADR_EXT = 0;
+	sf4.USR_RDLTC = 2;
+	sf4.USR_BUSY_EN = 1;
+	sf4.USR_CMD = 0xC7;
+	printf("set sf4 %x \n", sf4);
+	setDataRSP_(fd, SFLS_REG_BASE + (4 << 3), REG_SIZE, &sf4);
+
+	_SFLS_3 sf3;
+	getDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+	printf("get sf3 %x \n", sf3);
+	sf3.USR_RDREG_CMD_MODE = 2;
+	sf3.USR_RDREG_DAT_MODE = 2;
+	printf("set sf3 %x \n", sf3);
+	setDataRSP_(fd, SFLS_REG_BASE + (3 << 3), REG_SIZE, &sf3);
+
+	_SFLS_8 sf8;
+	getDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+	printf("get sf8 %x \n", sf8);
+	sf8.USR_REQ = 1;
+	printf("set sf8 %x \n", sf8);
+	setDataRSP_(fd, SFLS_REG_BASE + (8 << 3), REG_SIZE, &sf8);
+
+	sfls_vait_for_reg_request(fd);
+
+}
+
+
---
+++ Test_CLI/flash.h
@@ -0,0 +1,143 @@
+#ifndef _FLASH_H_
+#define _FLASH_H_
+
+#define BF_1(a1) \
+	a1;
+#define BF_2(a1,a2)\
+	a2; a1;
+#define BF_3(a1,a2,a3)\
+	a3; a2; a1;
+#define BF_4(a1,a2,a3,a4)\
+	a4; a3; a2; a1;
+#define BF_5(a1,a2,a3,a4,a5)\
+	a5; a4; a3; a2; a1;
+#define BF_6(a1,a2,a3,a4,a5,a6)\
+	a6; a5; a4; a3; a2; a1;
+#define BF_7(a1,a2,a3,a4,a5,a6,a7)\
+	a7; a6; a5; a4; a3; a2; a1;
+#define BF_8(a1,a2,a3,a4,a5,a6,a7,a8)\
+	a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_9(a1,a2,a3,a4,a5,a6,a7,a8,a9)\
+	a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_10(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10)\
+	a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_11(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11)\
+	a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_12(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12)\
+	a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_13(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13)\
+	a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_14(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14)\
+	a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_15(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15)\
+	a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_16(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16)\
+	a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_17(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17)\
+	a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_18(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18)\
+	a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_19(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19)\
+	a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_20(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20)\
+	a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_21(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21)\
+	a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_22(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22)\
+	a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_23(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23)\
+	a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_24(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24)\
+	a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_25(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25)\
+	a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_26(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26)\
+	a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_27(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27)\
+	a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_28(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28)\
+	a28; a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_29(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29)\
+	a29; a28; a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_30(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30)\
+	a30; a29; a28; a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_31(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31)\
+	a31; a30; a29; a28; a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define BF_32(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32)\
+	a32; a31; a30; a29; a28; a27; a26; a25; a24; a23; a22; a21; a20; a19; a18; a17; a16; a15; a14; a13; a12; a11; a10; a9; a8; a7; a6; a5; a4; a3; a2; a1;
+#define	_regs_					typedef	union{UINT a; struct{	// Register union start
+#define	_rege_					};}							// Register union end
+#define	_am(uon,base,addr)		((volatile uon*)(UINT*)(base+(addr)))->a		// All macro
+#define	_bm(uon,base,addr,mne)	((volatile uon*)(UINT*)(base+(addr)))->mne	// Bit macro
+
+_regs_ BF_3(UINT SFLS_ID : 24, UINT _rev0 : 7, UINT SFLS_ID_VAL : 1) _rege_ _SFLS_0;
+_regs_ BF_4(UINT BUS_CMD_RD : 8, UINT BUS_CMD_WR : 8, UINT BUS_CMD_WREN : 8, UINT BUS_CMD_RDREG : 8) _rege_ _SFLS_1;
+_regs_ BF_13(UINT BUS_RD_CMD_MODE : 2, UINT BUS_RD_ADR_MODE : 2, UINT BUS_RD_DAT_MODE : 2, UINT BUS_WR_CMD_MODE : 2, UINT BUS_WR_ADR_MODE : 2, UINT BUS_WR_DAT_MODE : 2, UINT BUS_WREN_CMD_MODE : 2, UINT BUS_RDREG_CMD_MODE : 2, UINT BUS_RDREG_DAT_MODE : 2, UINT BUS_ADR_EXT : 1, UINT BUS_GAP_EN : 1, UINT BUS_GAP : 4, UINT _rev0 : 8) _rege_ _SFLS_2;
+_regs_ BF_4(UINT USR_CMD_RDREG : 8, UINT USR_RDREG_CMD_MODE : 2, UINT USR_RDREG_DAT_MODE : 2, UINT _rev0 : 20) _rege_ _SFLS_3;
+_regs_ BF_14(UINT USR_CMD_MODE : 2, UINT USR_ADR_MODE : 2, UINT USR_DAT_MODE : 2, UINT USR_ADR_EN : 1, UINT USR_GAP_EN : 1, UINT USR_RD_EN : 1, UINT USR_WR_EN : 1, UINT USR_GAP : 4, UINT USR_LEN : 2, UINT USR_ADR_EXT : 1, UINT USR_BUSY_EN : 1, UINT _rev0 : 4, UINT USR_RDLTC : 2, UINT USR_CMD : 8) _rege_ _SFLS_4;
+_regs_ BF_1(UINT USR_ADR : 32) _rege_ _SFLS_5;
+_regs_ BF_1(UINT USR_RDDAT : 32) _rege_ _SFLS_6;
+_regs_ BF_1(UINT USR_WRDAT : 32) _rege_ _SFLS_7;
+_regs_ BF_12(UINT IO_RXEDGE : 1, UINT IO_TXEDGE : 1, UINT _rev0 : 4, UINT IO_RDLTC : 2, UINT _rev1 : 4, UINT IO_CLKDIV : 4, UINT _rev2 : 5, UINT IO_CS_GAP : 3, UINT _rev3 : 5, UINT USRBUS_REQ : 1, UINT BUS_REQ : 1, UINT USR_REQ : 1) _rege_ _SFLS_8;
+
+
+
+
+
+
+
+#define SFLS_SFLS_ID _bm(_SFLS_0,REG_BASE_SFLS, (0<<3),SFLS_ID) // 24 Bit, 24'h0, R
+#define SFLS_SFLS_ID_VAL _bm(_SFLS_0,REG_BASE_SFLS, (0<<3),SFLS_ID_VAL) // 1 Bit, 1'h0, R
+#define SFLS_BUS_CMD_RD _bm(_SFLS_1,REG_BASE_SFLS, (1<<3),BUS_CMD_RD) // 8 Bit, 8'h03, RW
+#define SFLS_BUS_CMD_WR _bm(_SFLS_1,REG_BASE_SFLS, (1<<3),BUS_CMD_WR) // 8 Bit, 8'h02, RW
+#define SFLS_BUS_CMD_WREN _bm(_SFLS_1,REG_BASE_SFLS, (1<<3),BUS_CMD_WREN) // 8 Bit, 8'h06, RW
+#define SFLS_BUS_CMD_RDREG _bm(_SFLS_1,REG_BASE_SFLS, (1<<3),BUS_CMD_RDREG) // 8 Bit, 8'h05, RW
+#define SFLS_BUS_RD_CMD_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_RD_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_RD_ADR_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_RD_ADR_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_RD_DAT_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_RD_DAT_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_WR_CMD_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_WR_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_WR_ADR_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_WR_ADR_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_WR_DAT_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_WR_DAT_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_WREN_CMD_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_WREN_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_RDREG_CMD_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_RDREG_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_RDREG_DAT_MODE _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_RDREG_DAT_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_BUS_ADR_EXT _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_ADR_EXT) // 1 Bit, 1'h0, RW
+#define SFLS_BUS_GAP_EN      _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_GAP_EN     ) // 1 Bit, 1'h0, RW
+#define SFLS_BUS_GAP         _bm(_SFLS_2,REG_BASE_SFLS, (2<<3),BUS_GAP        ) // 4 Bit, 4'h0, RW
+#define SFLS_USR_CMD_RDREG _bm(_SFLS_3,REG_BASE_SFLS, (3<<3),USR_CMD_RDREG) // 8 Bit, 8'h05, RW
+#define SFLS_USR_RDREG_CMD_MODE _bm(_SFLS_3,REG_BASE_SFLS, (3<<3),USR_RDREG_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_USR_RDREG_DAT_MODE _bm(_SFLS_3,REG_BASE_SFLS, (3<<3),USR_RDREG_DAT_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_USR_CMD_MODE _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_CMD_MODE) // 2 Bit, 2'h0, RW
+#define SFLS_USR_ADR_MODE    _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_ADR_MODE   ) // 2 Bit, 2'h0, RW
+#define SFLS_USR_DAT_MODE    _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_DAT_MODE   ) // 2 Bit, 2'h0, RW
+#define SFLS_USR_ADR_EN      _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_ADR_EN     ) // 1 Bit, 1'h0, RW
+#define SFLS_USR_GAP_EN      _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_GAP_EN     ) // 1 Bit, 1'h0, RW
+#define SFLS_USR_RD_EN _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_RD_EN) // 1 Bit, 1'h0, RW
+#define SFLS_USR_WR_EN _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_WR_EN) // 1 Bit, 1'h0, RW
+#define SFLS_USR_GAP _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_GAP) // 4 Bit, 4'h0, RW
+#define SFLS_USR_LEN _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_LEN) // 2 Bit, 2'h0, RW
+#define SFLS_USR_ADR_EXT _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_ADR_EXT) // 1 Bit, 1'h0, RW
+#define SFLS_USR_BUSY_EN _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_BUSY_EN) // 1 Bit, 1'h0, RW
+#define SFLS_USR_RDLTC _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_RDLTC) // 2 Bit, 2'h1, RW
+#define SFLS_USR_CMD _bm(_SFLS_4,REG_BASE_SFLS, (4<<3),USR_CMD) // 8 Bit, 8'h0, RW
+#define SFLS_USR_ADR _bm(_SFLS_5,REG_BASE_SFLS, (5<<3),USR_ADR) // 32 Bit, 32'h0, RW
+#define SFLS_USR_RDDAT _bm(_SFLS_6,REG_BASE_SFLS, (6<<3),USR_RDDAT) // 32 Bit, 32'h0, RW
+#define SFLS_USR_WRDAT _bm(_SFLS_7,REG_BASE_SFLS, (7<<3),USR_WRDAT) // 32 Bit, 32'h0, RW
+#define SFLS_IO_RXEDGE _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),IO_RXEDGE) // 1 Bit, 1'h0, RW
+#define SFLS_IO_TXEDGE _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),IO_TXEDGE) // 1 Bit, 1'h0, RW
+#define SFLS_IO_RDLTC _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),IO_RDLTC) // 2 Bit, 2'h1, RW
+#define SFLS_IO_CLKDIV _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),IO_CLKDIV) // 4 Bit, 4'hF, RW
+#define SFLS_IO_CS_GAP _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),IO_CS_GAP) // 3 Bit, 3'h7, RW
+#define SFLS_USRBUS_REQ _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),USRBUS_REQ) // 1 Bit, 1'h0, RW
+#define SFLS_BUS_REQ _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),BUS_REQ) // 1 Bit, 1'h0, RW
+#define SFLS_USR_REQ _bm(_SFLS_8,REG_BASE_SFLS, (8<<3),USR_REQ) // 1 Bit, 1'h0, RW
+
+
+
+void sfls_init(int fd);
+void sfls_sect_erase(int fd, UINT Adr);
+void sfls_block_erase(int fd, UINT Adr);
+void sfls_chip_erase(int fd);
+
+
+#endif
